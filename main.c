17d8e06caaa9a0134e41c074920adcfc401444f3
Sergey Kirikeza <serj280294@yandex.ru>
2018-10-17 17:57:18 +0400

    new version with structures

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
- #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <windows.h>
@@ -21,30 +21,25 @@
 #define MANYSYMBS 3
 #define FEWITEMS  4
 
-char** getContacts(int *opst);
-char* getString(FILE *fp, int *st, int *ef);
+typedef struct scts {
+    int n;
+} contstr;
+
+contstr** getContacts(int *res);
 void slfree(char **lst, int stramo);
 int lststrcmp(const void *a, const void *b);
 void toendstr(FILE *f, int *ef);
-int isspacesmb(char c);
-int isletter(char c);
-int isnumber(char c);
 
 int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    char **cns = NULL; // Указатель на массив строк, прочитанных из файла
-    int stgcs = 0;      // Переменная, хранящая число прочитанных строк или код ошибки
+    char **cts = NULL; // Указатель на массив строк, прочитанных из файла
+    int resgs = 0;     // Переменная, хранящая число прочитанных строк или код ошибки
     int i = 0;
 
-    if ((cns = getContacts(&stgcs)) == NULL) {
-        //if (stgcs == ERROR_OPEN_INPUT_FILE)
-        //    printf("Ошибка открытия входного файла");
-
-        return stgcs;
-    }
+    cts = getContacts(&resgs);
 
     //printlst(cns, stgcs); // Временно (!!!!!!!!!!!!!!!!!!!!)
 
@@ -55,81 +50,42 @@ int main()
     return 0;
 }
 
+// getContacts: возвращает указатель на массив строк или NULL, в случае ошибки
+// по адресу st помещает количество строк, содержащихся в массиве, или номер ошибки
+contstr** getContacts(int *res)
+{
+    FILE *csFile;            // Указатель на поток открытого файла
+    contstr **csList = NULL; // Список указателей на объекты структуры, хранящие записи файла
+    contstr *str = NULL;     // Указатель на объект структуры, хранящий строку телефонной книги
+    int enitem = 0;          // Временное хранилище пункта записи телефонной книги
+    int stcnt = 0;           // Счетчик строк, прочитанных из файла
+    int i = 0;
+
+    if ((csFile = fopen(CSFILE, READ_ONLY)) == NULL) {
+        *res = ERROR_OPEN_INPUT_FILE;
+        return NULL;
+    }
+
+    // Выделяем память для хранения указателей на объекты структуры
+    csList = (contstr **) malloc(15 * sizeof(contstr *));
+
+    while(fscanf(csFile, "%d", &enitem) != EOF) {
+        // Выделяем память для объекта структуры
+        csList[stcnt] = (contstr*) malloc(sizeof(contstr));
+        csList[stcnt]->n = enitem;
+        ++stcnt;
+    }
+
+    // Перераспределить память
+
+    *res = stcnt;
+    return csList;
+}
+
 // lststrcmp: сравнивает две строки из массива строк
 int lststrcmp(const void *a, const void *b)
 {
     return strcmp(*(char**)a, *(char**)b);
-}
-
-// getContacts: возвращает указатель на массив строк или NULL, в случае ошибки
-// по адресу st помещает количество строк, содержащихся в массиве, или номер ошибки
-char** getContacts(int *opst)
-{
-    FILE *csFile;     // Указатель на поток открытого файла
-    char **cslist;    // Массив указателей на строки записной книги
-    int coalloc = 1;  // Множитель количества ячеек выделяемой памяти под строки телефонной книги
-    int stgs = 0;     // Переменная для хранения кода результата выполнения операции чтения строки
-    int fend = 0;     // Переменная-флаг завершения файла
-    char *str = NULL; // Указатель на строку
-    int i = 0, j = 1;
-
-    if ((csFile = fopen(CSFILE, READ_ONLY)) == NULL) {
-        *opst = ERROR_OPEN_INPUT_FILE;
-        return NULL;
-    }
-
-    // Выделяем память для хранения ENTRYSAMO указателей на строки
-    cslist = (char**) malloc(ENTRYSAMO * sizeof(char*));
-
-    // Заполняем массив указателей адресами строк
-    str = getString(csFile, &stgs, &fend);
-    while (!(fend)) {
-        if (str) {
-
-            printf("S%d: %s %d\n", j, str, strlen(str));
-
-            if (i == ENTRYSAMO * coalloc) { // Превышен размер массива указателей на строки
-                ++coalloc; // Увеличиваем множитель количества ячеек
-                // Перераспределяем массив с указанием нового размера
-                cslist = (char**)realloc(cslist, ENTRYSAMO * coalloc * sizeof(char*));
-            }
-            *(cslist + i) = str; // Помещаем адрес строки в ячейку
-            ++i;
-        }
-        else {
-            printf("Err S%d: ", j);
-            switch (stgs) {
-            case MANYSYMBS:
-                printf("Превышено макcимально возможное количество символов в строке");
-                break;
-            case MANYWORDS:
-                printf("Превышено ожидаемое количество слов в записи");
-                break;
-            case MANYNUMBS:
-                printf("Превышено макcимально возможное количество номеров");
-                break;
-            case FEWITEMS:
-                printf("В строке содержится меньшее количество пунктов, чем ожидалось");
-                break;
-            }
-            putchar('\n');
-        }
-
-        str = getString(csFile, &stgs, &fend);
-        ++j;
-    }
-
-    // Если цикл завершился не из-за окончания файла - ошибка
-    if (stgs == ENDFILE) {
-        *opst = i; // Размещаем количество строк, прочитанных из файла
-        // Возвращаем адрес массива указателей на строки, сократив число незанятых элементов
-        return (char**)realloc(cslist, i * sizeof(char*));
-    }
-    else {
-        *opst = stgs;
-        slfree(cslist, i); //  Освободить каждую строку и массив указателей
-        return NULL;
-    }
 }
 
 // Возвращает указатель на область данных, содержащую строку или NULL, в случае ошибки или конца файла
@@ -146,7 +102,6 @@ char* getString(FILE *fp, int *st, int *ef)
     // Выделяем память под строку
     str = (char*) malloc(BUFSTRLEN * sizeof(char));
 
-
     while ((c = fgetc(fp)) != EOF && c != '\n') {
         if (i == BUFSTRLEN-1) {
             toendstr(fp, ef);
@@ -154,13 +109,14 @@ char* getString(FILE *fp, int *st, int *ef)
             free(str);
             return NULL;
         }
-        if (isspacesmb(c)) {
+
+        if (isspace(c)) { // Заменить!!!
             if (i != 0 && !(isspace(str[i-1])) && stitms < WDSBFNUM+1) {
                 str[i] = ' ';
                 ++i;
             }
         }
-        else if (isletter(c)) {
+        else if (isalpha(c)) { // Заменить на isletter
             if (i == 0 || isspace(str[i-1]))
                 ++stitms;
 
@@ -173,14 +129,13 @@ char* getString(FILE *fp, int *st, int *ef)
             str[i] = c;
             ++i;
         }
-        else if (isnumber(c)) {
-            if (isspace(str[i-1]) && stitms == WDSBFNUM) {
+        else if (isdigit(c)) { // Заменить на isnumber
+            if (isspace(str[i-1]) && stitms == WDSBFNUM) { // Начало номера
                 ++stitms;
             }
-
             else if (stitms > WDSBFNUM+1) {
                 toendstr(fp, ef);
-                *st = MANYNUMBS;
+                *st = MANYNUMBS; // Номер уже был прочитан
                 free(str);
                 return NULL;
             }
@@ -217,7 +172,7 @@ int isletter(char c)
            (c >= (unsigned char)'A' && c <= (unsigned char)'z') ? 1 : 0;
 }
 
-// isnumber: Проверяет, относится ли символ к номеру
+// isnumber: Проверяет, является ли символ пробельным
 int isnumber(char c)
 {
     return ((c >= 0 && c <=9) || c == '+' || c == '-' || c == '(' || c == ')') ? 1 : 0;
